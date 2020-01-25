23ed35dd267cafa6d6b030b00aba559d71999dcc
ilham88 <iam.klopers@gmail.com>
2020-01-25 13:52:38 +0700

    Update credentials.py

--- modules/credentials.py
+++ modules/credentials.py
@@ -1,6 +1,6 @@
 class Creds(object):
-    TOKEN = "1004798045:AAGNshtc0UQEqhV24LG-CqsOUOCdwQJJCY0"
-    CLIENT_ID = "286396467948-h1rbjljlqq1tb18iigbdnjdjvn42hr7i.apps.googleusercontent.com"
-    CLIENT_SECRET = "lSj6KggOkT556Dzu-z0p8Tde"
-    PATH_ID = "10lRmQ973PD5Hc_mXFD-kNL5kQL4xiCl1"
+    TOKEN = "1095208510:AAGI7hZV78frk9Y_YdBOluhXYcxf9KVYjWU"
+    CLIENT_ID = "882645358486-vk3a1hb008gg4f2978ou7dh65ig8r8qb.apps.googleusercontent.com"
+    CLIENT_SECRET = "uUi4hUN87HBkk3Xqs1_Y3bPX"
+    PATH_ID = "1Bo4DZOhVjDDkABlEpqX2YCJvYii2sSAu"
     USER_ID = "684032767"
